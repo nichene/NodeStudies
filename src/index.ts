a9a610d19c938058d98cc442df56faf992eee4bd
ni <nichenevercosa@gmail.com>
2020-06-21 18:51:11 -0300

    initial connect to db

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import graphqlHTTP from "express-graphql";
+const schema = require("./schema/schema");
+const app = express();
+
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://SmUser:SmPass@localhost:27017/SmartFit");
+
+mongoose.connection.once("open", () => {
+  console.log("conneted to database");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Database error: " + err);
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
